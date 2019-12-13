97b6a23b70e3b1ee4077fbb4e78e9dc1ae579504
Shainu Prakash <shainu@Shainus-MacBook-Pro.local>
2019-12-12 19:41:34 -0500

    lets see

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert(1);
+var channelName = 'HowToPlayCA';
+
+$(document).ready(function()
+                 {
+    $.get(
+        "https://www.googleapis.com/youtube/v3/channels",{
+            part: 'contentDetails',
+            id: channelName,
+            key: 'AIzaSyBwiAp0jc-W7Z2NhjoRsDgvGogcgW-XTLg'},
+            function(data){
+                $.each(data.items, function(i,item){
+                    console.log(item);
+                    pid = item.contentDetails.relatedPlaylists.uploads;
+                    getVids(pid);
+                })
+            }
+       );
+    
+});

06c0e2f7b20dadff42db88858a7c1ff39d098e54
Shainu Prakash <shainu@Shainus-MacBook-Pro.local>
2020-12-27 11:58:50 -0500

    fixed api

---
+++ config.js
@@ -0,0 +1,2 @@
+var apiKey='AIzaSyC1qeQOUKy7dNZPbiv0U_x9N3UZqQNqzRc';
+var channelId='UC1VDpWpOf36CuP9fowyDZtQ'; //channel id
--- images/youtube.js
+++ images/youtube.js
@@ -1,5 +1,5 @@
-var apiKey='AIzaSyC1qeQOUKy7dNZPbiv0U_x9N3UZqQNqzRc';
-var channelId='UC1VDpWpOf36CuP9fowyDZtQ'; //channel id
+var apiKey=config.apiKey;
+var channelId= config.channelId;
 
 $(document).ready(function(){
 
